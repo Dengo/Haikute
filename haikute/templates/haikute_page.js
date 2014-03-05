ae6cf666424df236d638c35ba69da730eee25b94
markcharyk <markcharyk@gmail.com>
2014-03-05 13:48:26 -0800

    my hands are typing words

--- haikute/templates/haikute_page.js
+++ haikute/templates/haikute_page.js
@@ -30,8 +30,9 @@ $(document).ready(function () {
             timeout: 10000,
             dataType: 'json',
             success: function(response) {
-                five_haikus = response.getJSONArray("haiqueue");
+                var five_haikus = response.getJSONArray("haiqueue");
                 if (five_haikus.getString(0) != $('#prev-haikus').find('li').first()) {
+                    $('#prev-haikus').find('li').remove();
                     $('#prev-haikus').append($('<li>'+five_haikus.getString(0)+'</li>'));
                     $('#prev-haikus').append($('<li>'+five_haikus.getString(1)+'</li>'));
                     $('#prev-haikus').append($('<li>'+five_haikus.getString(2)+'</li>'));
