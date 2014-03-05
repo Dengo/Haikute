c74d33f018df1ec08427e8022791e4f957e310cb
markcharyk <markcharyk@gmail.com>
2014-03-05 11:58:41 -0800

    Figuring out JSON handling

--- haikute/templates/haikute_page.js
+++ haikute/templates/haikute_page.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
     $('button').on('click', function() {
-        var haiku = $(this).closest('#current-haiku').find('p');;
+        var haiku = $(this).closest('#current-haiku').find('p');
         $.ajax({
             type: 'GET',
-            url: '/haiku',
+            url: '/api/haiku',
             //data: 'Please remit haiku post haste',
             dataType: 'json',
             success: function(response) {
                 haiku.remove();
                 var hk = $("<p></p>");
-                hk.append(response);
-                $('body').html(haiku);
+                hk.append(response.getString("haiku"));
+                $('#current-haiku').prepend(hk);
             },
             error: function(request, errorType, errorMessage) {
-                alert('Error: ' + request + ' with message ' + errorMessage);
+                haiku.remove();
+                var hk = $("<p></p>");
+                hk.append("No haiku fetched!");
+                $('#current-haiku').prepend(hk);
             },
         });
     });
@@ -21,17 +24,26 @@ $(document).ready(function () {
     var longPoll = function() {
         return $.ajax({
             type: 'GET',
-            url: '/haiqueue',
-            async: true,
-            cache: false,
+            url: '/api/haiqueue',
+            // async: true,
+            // cache: false,
             timeout: 10000,
+            dataType: 'json',
             success: function(response) {
-                if (response.length > 0) {
-                    $('#prev-haikus').append($('<li>'+response+'</li>'))
+                five_haikus = response.getJSONArray("haiqueue");
+                if (five_haikus.getString(0) != $('#prev-haikus').find('li').first()) {
+                    $('#prev-haikus').append($('<li>'+five_haikus.getString(0)+'</li>'));
+                    $('#prev-haikus').append($('<li>'+five_haikus.getString(1)+'</li>'));
+                    $('#prev-haikus').append($('<li>'+five_haikus.getString(2)+'</li>'));
+                    $('#prev-haikus').append($('<li>'+five_haikus.getString(3)+'</li>'));
+                    $('#prev-haikus').append($('<li>'+five_haikus.getString(4)+'</li>'));
                 }
                 return longPoll();
             },
-            dataType: 'json'
+            error: function(request, errorType, errorMessage) {
+                $('#prev-haikus').find('li').remove();
+                $('#prev-haikus').append($('<li>'+'No previous haikus found!'+'</li>'));
+            },
         });
     };
 
