1d214bb9be5c40d8ae0868f9fd1374241d7db02c
markcharyk <markcharyk@gmail.com>
2014-03-04 21:29:35 -0800

    Add proper ajax maybe

--- haikute/templates/haikute_page.js
+++ haikute/templates/haikute_page.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     $('button').on('click', function() {
-        var haiku = $(this).closest('body');
+        var haiku = $(this).closest('body').find('p');;
         $.ajax({
-            url: 'haiku_text_page.html',
-            data: 'button=' + $(this).val(),
-            datatype: 'json',
+            type: 'POST',
+            url: 'http://localhost:8000/haiku_text_page.html',
+            data: 'Please remit haiku post haste',
+            dataType: 'json',
             success: function(response) {
-                $('p').html(response);
+                haiku.remove();
+                var hk = $("<p></p>");
+                hk.append(response);
+                $('body').hide().html(haiku).fadeIn();
             },
             error: function(request, errorType, errorMessage) {
                 alert('Error: ' + request + ' with message ' + errorMessage);
