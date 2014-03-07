502ab0e9a93874602fbd93b243f62f2dec6fdb6b
Stephen Babineau <tien_ralter@hotmail.com>
2014-03-07 11:34:57 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=['PyYAML >= 3.10',
                       'argparse >= 1.2.1',
                       'gunicorn >= 18.0',
-                      'nltk==2.0.4'
+                      'nltk==2.0.4',
+                      'lea==1.2',
                       ]
 )
